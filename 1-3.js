f1cdabdfa70a63ab4171f62898fa15f308b17185
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-08-22 09:49:04 +0900

    몫구하기

---
+++ 1-3.js
@@ -0,0 +1,10 @@
+function solution(age) {
+    
+    var born = 2023-age;
+    return born;
+}
+teacher1=solution(40);
+teacher2=solution(23);
+console.log(teacher1);
+console.log(teacher2);
+//나이 출력
