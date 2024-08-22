7a97e0d19049ff5f2534bff35d773e938b8a17b7
Jacob-moon <53264081+Jacob-moon@users.noreply.github.com>
2024-08-22 09:56:00 +0900

    숫자 비교하기

---
+++ 1-5.js
@@ -0,0 +1,14 @@
+function solution(num1, num2) {
+    if(num1===num2){
+        return 1;
+        
+    }else if (num1!==num2){
+        return  -1
+    }
+  
+    return ;
+}
+console.log(solution(2,3));
+console.log(solution(11,11));
+console.log(solution(7,99));
+//숫자 비교하기
