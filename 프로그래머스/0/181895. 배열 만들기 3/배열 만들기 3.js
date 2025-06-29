7518d29294645cf34dde0703eb38d58182908199
Jacob_moon <53264081+Jacob-moon@users.noreply.github.com>
2025-06-29 22:23:54 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.79 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,9 @@
+function solution(arr, intervals) {
+  const [a1, b1] = intervals[0];
+  const [a2, b2] = intervals[1];
+
+  const firstPart = arr.slice(a1, b1 + 1);
+  const secondPart = arr.slice(a2, b2 + 1);
+
+  return firstPart.concat(secondPart);
+}
